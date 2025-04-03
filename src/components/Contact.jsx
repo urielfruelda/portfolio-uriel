1e1f6cf24f74e52c560d855b28f0040579c1e494
Ernan Uriel Fruelda <119768070+urielfruelda@users.noreply.github.com>
2025-04-03 10:41:10 +0800

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
                         <p className="text-xs md:text-base"><span>Projects</span></p>
                     </div>
                     <div className="bg-gray-800/40 p-4 rounded-lg">
-                        <h3 className="md:text-4xl text-2xl font-semibold text-white">4
+                        <h3 className="md:text-4xl text-2xl font-semibold text-white">5
                             <span>+</span>
                         </h3>
                         <p className="text-xs md:text-base"><span>Certifications</span></p>
